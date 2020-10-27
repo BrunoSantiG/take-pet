1a6ccd96bba8a0105dda69e8a750210123d42f33
Bruno Santi Garcia <bruno.santi.98@outlook.com>
2020-10-27 11:11:47 -0300

    [+] Adiciona filtro de produtos

--- src/controller/produtos.js
+++ src/controller/produtos.js
@@ -1,3 +1,21 @@
+const GatoFile = '../mock-data/produtos-cat.json'
+const CachorroFile = '../mock-data/produtos-dog.json'
+ 
 export function getProdutos(req,res){
-const pet=req.query.animal
+    const pet=req.params.animal
+    let arquivo
+    if(pet==='cachorro'){
+        produtos = CachorroFile.produtos
+    }else if(pet==='gato'){
+        produtos = GatoFile.produtos
+    }
+
+    const produtos_filter = produtos.filter((value)=>{
+        return produtos.nome.toLowerCase().include(req.params.pesquisa.toLowerCase())
+    })
+
+    return res.status(200).json({
+        produtos_filter
+    })
+
 }
